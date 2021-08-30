0048441d00f41d1143e53bfc927c880544471274
Coreywilson319 <coreywilson319@gmail.com>
2021-08-30 19:49:21 -0400

    npm init

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,7 @@ import RastaImagery from "../resources/rasta_imagery.png"
 
 const Home = () => {
     return (
-        <div>
-            
+        <div id="body">
                 <img id="portrait" src="https://avatars.githubusercontent.com/u/72322079?s=400&u=ca5a1b6ce956fce8d8945fe18e83fb98cdba8dbc&v=4"></img>
                 <h1 id="name">
                     Corey Wilson
